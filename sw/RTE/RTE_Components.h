aeec332bfd8d8d597ef4d439d89f416b3963b617
Marko Pavlin <marko@pavlin.si>
2014-06-30 01:29:53 +0200

    Initial commit

---
+++ sw/RTE/RTE_Components.h
@@ -0,0 +1,23 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'galwii' 
+ * Target:  'GalWii' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STDPERIPH_ADC
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+#define RTE_DEVICE_STDPERIPH_GPIO
+#define RTE_DEVICE_STDPERIPH_RCC
+#define RTE_DEVICE_STDPERIPH_TIM
+#define RTE_Drivers_USBD0               /* Driver USBD0 */
+#define RTE_USB_Core                    /* USB Core */
+#define RTE_USB_Device_0                /* USB Device 0 */
+#define RTE_USB_Device_CDC_0            /* USB Device CDC instance 0 */
+
+#endif /* RTE_COMPONENTS_H */
